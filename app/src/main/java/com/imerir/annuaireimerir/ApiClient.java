e3a541dda4787f34fcc82755a2bf9a1b700c0d51
Tristan Wagner <t.wagner66250@gmail.com>
2017-01-10 09:35:12 +0100

    SplashScreen + ApiClient & App
    À vérifier si le splashscreen est bien fonctionnel

---
+++ app/src/main/java/com/imerir/annuaireimerir/ApiClient.java
@@ -0,0 +1,29 @@
+package com.imerir.annuaireimerir;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by student on 10/01/2017.
+ */
+
+public class ApiClient {
+    private static ApiClient instance;
+    Context context;
+    RequestQueue queue;
+    int success;
+
+
+    public ApiClient(Context context) {
+        this.context = context;
+        queue = Volley.newRequestQueue(context);
+    }
+
+    public static void createInstance(Context context) {
+        instance = new ApiClient(context);
+    }
+
+    public static ApiClient getInstance() {return instance;}
+}
